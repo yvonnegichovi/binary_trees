0d733575ece84ca4b32f2d8bb93bcc7abcfe8067
yvonnegichovi <yvonnegichovi@gmail.com>
2024-01-30 10:02:35 -0800

    task 14

--- binary_trees.h
+++ binary_trees.h
@@ -48,7 +48,7 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-
+size_t binary_tree_height(const binary_tree_t *tree);
 
 /* advanced tasks, prototypes in order of the tasks */
 
