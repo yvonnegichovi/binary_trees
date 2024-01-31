c7578dd531762fd802f9b3f256a244b843d60ed9
yvonnegichovi <yvonnegichovi@gmail.com>
2024-01-31 09:57:14 -0800

    taks 20

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -15,21 +15,21 @@
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
 	int front = 0, rear = 0;
-	const binary_tree_t **queue = NULL;
+	binary_tree_t **queue = NULL;
 	const binary_tree_t *current = NULL;
 
 	queue = malloc(sizeof(binary_tree_t *) * 1024);
 	if (queue == NULL)
 		return;
-	queue[rear++] = tree;
+	queue[rear++] = (binary_tree_t *)tree;
 	while (front < rear)
 	{
 		current = queue[front++];
 		func(current->n);
 		if (current->left != NULL)
-			queue[rear++] = current->left;
+			queue[rear++] = (binary_tree_t *)current->left;
 		if (current->right != NULL)
-			queue[rear++] = current->right;
+			queue[rear++] = (binary_tree_t *)current->right;
 	}
 	free(queue);
 }
