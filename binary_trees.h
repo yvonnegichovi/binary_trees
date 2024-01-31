92de0d5e046af42644fa42022119661c281d68cc
yvonnegichovi <yvonnegichovi@gmail.com>
2024-01-31 10:43:27 -0800

    task 22

--- binary_trees.h
+++ binary_trees.h
@@ -54,6 +54,7 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
 int is_complete(const binary_tree_t *tree, size_t index, size_t num_nodes);
 size_t count_nodes(const binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 
 /* advanced tasks, prototypes in order of the tasks */
 
