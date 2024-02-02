1b098b210e53f19e648fa085c19f1efcd5cd6f49
kaymacharia <kaymachariaa@gmail.com>
2024-02-01 22:39:35 -0800

    1

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -10,12 +10,6 @@
 int binary_tree_is_root(const binary_tree_t *node)
 {
 if (node == NULL)
-{
 return (0);
-}
-if (node->parent == NULL) {
-    return (1);
-} else {
-    return (0);
-}
+return (1);
 }
