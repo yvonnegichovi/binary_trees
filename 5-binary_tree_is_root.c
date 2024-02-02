1781180739ebc008dec84a2d0dc72de633675056
kaymacharia <kaymachariaa@gmail.com>
2024-02-01 22:25:23 -0800

    1

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,13 +1,17 @@
 #include "binary_trees.h"
 /**
- * binary_tree_is_root - function takes a pointer to a node (const binary_tree_t *node) as an argument.
+ * binary_tree_is_root - function takes a pointer to a node
+ * (const binary_tree_t *node) as an argument.
  * @node: the function checks if the node has no parent (node->parent == NULL)
- * Return:  If the node has no parent, it is considered a root, and the function returns 1.
+ * Return:  If the node has no parent, it is considered 
+ * a root, and the function returns 1.
  *  Otherwise :it returns 0.
  */
-int binary_tree_is_root(const binary_tree_t *node) {
-    if (node == NULL) {
-        return 0;
-    }
-    return (node->parent == NULL) ? 1 : 0;
+int binary_tree_is_root(const binary_tree_t *node)
+{
+if (node == NULL) 
+{
+return 0;
+}
+return (node->parent == NULL) ? 1 : 0;
 }
